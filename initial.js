f0fd832dc886f159cffc5bff3b25ff5af403c3ed
roikawa55 <61040017+roikawa55@users.noreply.github.com>
2021-11-17 16:44:34 -0800

    created player.js
    contains player struct and setters for player attributes (and a tiny test)

--- initial.js
+++ initial.js
@@ -1,2 +1,5 @@
 //this is just an initial file required for some reason. <- UNCLEAR COMMENT!
+// We use underscore for variable name conention.
 
+// REM: Used to include explanatory remarks in the source code of a program.
+// DIM: Declares and allocates storage space for one or more variables.
