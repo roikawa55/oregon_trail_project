c95ffc03eb2f4326bb0ce69086ab5b228469d34e
roikawa55 <61040017+roikawa55@users.noreply.github.com>
2021-11-26 10:49:50 -0800

    Update player.js
    Changed the structure of player data structure; player is now a class. A field 'ammo' is renamed to 'bullets'. A new field oxen is added. All the class members became private.

--- player.js
+++ player.js
@@ -1,160 +1,166 @@
-/* struct */
-var player = {
-    name: 'unnamed',
-    rifle_skill: 0,
-    cash: 700,
-    supplies: {food: 0, ammo: 0, clothes: 0},
-    health: {hurt: false, sick: false}
-}
+class player{    
+    #name = 'undefined'
+    #oxen = 0
+    #rifle_skill = 0
+    #supplies = {food: 0, bullets: 0, clothes: 0}
+    #health= {hurt: false, sick: false}
+    #cash = 700
+    
+    //#attribute_types = {name_type: 'string'};
 
-function input_type_mismatch_exception(required, actual){
-    console.log("Required type is: " + required + " and actual type is: " + actual)
-}
+    constructor(name, oxen, rifle_skill, food, bullets, clothes){
+        this.set_name(name)
+        this.stock_oxen(oxen)
+        this.set_rifle_skill(rifle_skill)
+        this.stock_food(food)
+        this.stock_bullets(bullets)
+        this.add_clothes(clothes)
+        this.debit(this.#name + this.#oxen + this.#supplies.food + this.#supplies.bullets + this.#supplies.clothes)
+    }
+    
+    input_type_mismatch_exception(required, actual){
+        console.log("Required type is: " + required + " and actual type is: " + actual)
+    }
 
-function set_name(new_name){
-    if(typeof(new_name) == "string"){
-        player.name = new_name
+    health_category_not_found_exception(category){
+        console.log("Health category " + category + " does not match either hurt or sick")
     }
-    else {
-        throw new input_type_mismatch_exception(typeof String, typeof new_name)
+     
+    check_type_match(arg, required){
+        if(typeof(required) != "string"){
+            throw new input_type_mismatch_exception('string', typeof required)
+        }
+        if(typeof(arg) != required)
+            throw new input_type_mismatch_exception(required, typeof arg)
     }
-}
 
-function change_rifle_skill(new_rifle_skill){
-    if(Number.isInteger(new_rifle_skill)){
-        player.rifle_skill = new_rifle_skill
+    set_name(new_name){
+        if(typeof(new_name) == "string"){
+            this.#name = new_name
+        }
+        else {
+            throw new input_type_mismatch_exception(typeof String, typeof new_name)
+        }
     }
-    else{
-        throw new input_type_mismatch_exception('integer', typeof new_rifle_skill)
-    }
-}
 
-function debit(money) {
-    if(player.cash == 0){
-        console.log('No money left')
+    stock_oxen(oxen){
+        check_type_match(oxen, "number")
+        this.#oxen += oxen
     }
-    else if(typeof(money) == 'number') {
-        //check for the money left
-        player.cash -= money
-    }
-    else{
-        throw new input_type_mismatch_exception('number', typeof money)
-    }
-}
 
-function stock_food(amount) {
-    if(typeof(amount) == 'number') {
-        player.supplies.food += amount
+    lose_oxen(oxen){
+        if(typeof(oxen) == 'number') {
+            this.#oxen -= oxen
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof oxen)
+        }
     }
-    else{
-        throw new input_type_mismatch_exception('number', typeof amount)
-    }
-}
 
-function consume_food(amount) {
-    if(player.supplies.food == 0){
-        console.log('No food left')
+    set_rifle_skill(new_rifle_skill){
+        if(Number.isInteger(new_rifle_skill)){
+            this.#rifle_skill = new_rifle_skill
+        }
+        else{
+            throw new input_type_mismatch_exception('integer', typeof new_rifle_skill)
+        }
     }
-    else if(typeof(amount) == 'number') {
-        player.supplies.food -= amount
+    
+    debit(money) {
+        if(this.#cash == 0){
+            console.log('No money left')
+        }
+        else if(typeof(money) == 'number') {
+            this.#cash -= money
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof money)
+        }
     }
-    else{
-        throw new input_type_mismatch_exception('number', typeof amount)
+    
+    stock_food(amount) {
+        if(typeof(amount) == 'number') {
+            this.#supplies.food += amount
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof amount)
+        }
     }
-}
+    
+    consume_food(amount) {
+        if(this.#supplies.food == 0){
+            console.log('No food left')
+        }
+        else if(typeof(amount) == 'number') {
+            this.#supplies.food -= amount
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof amount)
+        }
+    }
+    
+    stock_bullets(amount) {
+        if(typeof(amount) == 'number') {
+            this.#supplies.bullets += amount
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof amount)
+        }
+    }
+    
+    consume_bullets(amount) {
+        if(this.#supplies.bullets == 0){
+            console.log('No ammos left')
+        }
+        else if(typeof(amount) == 'number') {
+            this.#supplies.bullets -= amount
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof amount)
+        }
+    }
+    
+    add_clothes(pair) {
+        if(typeof(pair) == 'number') {
+            this.#supplies.clothes += pair
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof pair)
+        }
+    }
+    
+    remove_clothes(pair) {
+        if(this.#supplies.clothes == 0){
+            console.log('No clothes left')
+        }
+        else if(typeof(pair) == 'number') {
+            this.#supplies.clothes -= pair
+        }
+        else{
+            throw new input_type_mismatch_exception('number', typeof pair)
+        }
+    }
 
-function stock_ammo(amount) {
-    if(typeof(amount) == 'number') {
-        player.supplies.ammo += amount
+    stock_by_attribute(attribute, amount){
+    
     }
-    else{
-        throw new input_type_mismatch_exception('number', typeof amount)
-    }
-}
 
-function use_ammo(amount) {
-    if(player.supplies.ammo == 0){
-        console.log('No ammos left')
-    }
-    else if(typeof(amount) == 'number') {
-        player.supplies.ammo -= amount
-    }
-    else{
-        throw new input_type_mismatch_exception('number', typeof amount)
-    }
-}
+    consume_by_attribute(attribute, amount){
 
-function add_clothes(pair) {
-    if(typeof(pair) == 'number') {
-        player.supplies.clothes += pair
     }
-    else{
-        throw new input_type_mismatch_exception('number', typeof pair)
+    
+    change_health_condition(category, condition){
+        if(typeof(category) != 'string' || typeof(condition) != 'boolean'){
+            throw new input_type_mismatch_exception('boolean', typeof condition)
+        }
+        else if(category == 'hurt'){
+            this.#health.hurt = condition
+        }
+        else if(category == 'sick'){
+            this.#health.sick = condition
+        }
+        else{
+            throw new health_category_not_found_exception(category)
+        }
     }
-}
-
-function remove_clothes(pair) {
-    if(player.supplies.clothes == 0){
-        console.log('No clothes left')
-    }
-    else if(typeof(pair) == 'number') {
-        player.supplies.clothes -= pair
-    }
-    else{
-        throw new input_type_mismatch_exception('number', typeof pair)
-    }
-}
-
-function change_hurt_condition(condition){
-    if(typeof(condition) == 'boolean'){
-        player.health.hurt = condition
-    }
-    else {
-        throw new input_type_mismatch_exception('boolean', typeof condition)
-    }
-}
-
-function change_sick_condition(condition){
-    if(typeof(condition) == 'boolean'){
-        player.health.sick = condition
-    }
-    else {
-        throw new input_type_mismatch_exception('boolean', typeof condition)
-    }
-}
-
-
-
-try{
-    console.log('A Tiny Test WITHOUT ERROR/EXCEPTION')
-    console.assert(player.name == 'unnamed')
-    set_name('momo')
-    console.assert(player.name == 'momo')
-    console.assert(player.rifle_skill == 0)
-    change_rifle_skill(5)
-    console.assert(player.rifle_skill == 5)
-    console.assert(player.cash == 700)
-    debit(70)
-    console.assert(player.cash == 630)
-    console.assert(player.supplies.food == 0)
-    stock_food(100)
-    console.assert(player.supplies.food == 100)
-    consume_food(50)
-    console.assert(player.supplies.food == 50)
-    console.assert(player.supplies.ammo == 0)
-    stock_ammo(200)
-    console.assert(player.supplies.ammo == 200)
-    use_ammo(50)
-    console.assert(player.supplies.ammo == 150)
-}catch(input_type_mismatch_exception){
-    console.log("ALERT:: exception happend")
-}
-
-try {
-    console.log('A Tiny Test WITH EXCEPTION')
-    set_name(2)
-    change_rifle_skill(6.6)
-    debit('ababab')
-} catch (input_type_mismatch_exception) {
-    console.log("exception is handled")
 }
