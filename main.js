cfce8ccaffeebd867f9af6620cabd8ff38b7579e
michaeltothewilson <56896666+michaeltothewilson@users.noreply.github.com>
2021-11-29 00:43:23 -0800

    made a typewriter effect - only works on index

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function typewriter(){
+    let game_window = document.getElementById("gameWindow")
+    let p_element_object = game_window.getElementsByTagName('p')
+    let p_element_array = Array.from(p_element_object)
 
+    p_element_array.forEach((p,i) =>{
+        p.style.animationDelay= `${i}s`
+    })
+}
 
-
+typewriter()
