e033ef905c522d52a6f077088c55b6aa944964f4
michaeltothewilson <56896666+michaeltothewilson@users.noreply.github.com>
2021-12-04 01:33:32 -0800

    implemented form get post and variable passing

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const home_router = require('./routes/home')
-app.use('/', home_router)
-
 const instructions_1 = require('./routes/instructions_1')
-app.use('/instructions_1', instructions_1)
-
 const instructions_2 = require('./routes/instructions_2')
-app.use('/instructions_2', instructions_2)
-
 const instructions_3 = require('./routes/instructions_3')
-app.use('/instructions_3', instructions_3)
-
 const player_setup = require('./routes/player_setup')
-app.use('/player_setup', player_setup)
-
 const player_status = require('./routes/player_status')
+
+app.use('/', home_router)
+app.use('/instructions_1', instructions_1)
+app.use('/instructions_2', instructions_2)
+app.use('/instructions_3', instructions_3)
+app.use('/player_setup', player_setup)
 app.use('/player_status', player_status)
 
-const path = require('path')
-app.use(express.static('public'))
-// app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'pug')
+app.post('/player_setup', (req, res) => {
+    console.log('name' + JSON.stringify(req.body))
+    let player_input = req.body 
+    console.log(player_input)
+    res.render('player_status', { player_input })
+})
 
 app.listen(port, () => {
     console.log("Server Started")
--- routes/player_setup.js
+++ routes/player_setup.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.render('player_setup')
+    res.render('player_setup', {
+        data: {},
+        errors: {}
+    })
 })
 
 module.exports = router
