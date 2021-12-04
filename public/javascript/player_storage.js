91a707ce3b95c7013b9ad9e94e39593c9ec73936
roikawa55 <61040017+roikawa55@users.noreply.github.com>
2021-12-03 20:28:17 -0800

    created player_storage.js for storing and retrieving player status.

---
+++ public/javascript/player_storage.js
@@ -0,0 +1,17 @@
+
+const pug = require('pug')
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
+
+function set_item(key, value){
+    localStorage.setItem(key, JSON.stringify(value))
+    var status = JSON.parse(localStorage.getItem(key))
+    pug.renderFile("player_status", status)
+}
+
+function get_item(key){
+    //res.render("player_status", JSON.parse(localStorage.getItem(key)))
+}
